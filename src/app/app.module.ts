e6bb828a1fbc959ea2c49f0892022029af3f5487
David <dr3947@gmail.com>
2020-10-05 11:17:22 -0600

    Creacion de modulo home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { CustomerModule } from './customer/customer.module';
+import { HomeModule } from './home/home.module';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { CustomerModule } from './customer/customer.module';
     BrowserModule,
     AppRoutingModule,
     SharedModule,
-    CustomerModule
+    CustomerModule,
+    HomeModule
     
   ],
   providers: [],
