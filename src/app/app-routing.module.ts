75b981e8099fcc4617b2a61c1359f0b713a7e8c5
David <dr3947@gmail.com>
2020-10-10 23:20:08 -0600

    routing for home and customer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+   // loadChildren: './home/homemodule#HomeModule'
+   loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'customer',
+    loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule)
+  },
+  {
+    path: '', redirectTo: '/home', pathMatch:'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/customer/customer-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+
+
+const customerRoutes: Routes = [
+  {
+    path: '',
+    children:[
+      {
+        path: '',
+        component : CustomerListComponent
+      }
+    ]
+  }
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(customerRoutes)
+  ],
+  exports: [RouterModule]
+})
+export class CustomerRoutingModule { }
